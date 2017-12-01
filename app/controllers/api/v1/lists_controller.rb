81060d789fff7ca5cc09d05a9e7604fa5dadee61
Tyler Wanlass <tyler@buffer.com>
2017-12-01 06:44:15 -0800

    Return lists as flat collection of ids for Redux on the client.

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -3,7 +3,11 @@ module Api::V1
     before_action :authenticate_user
 
     def index
-      render json: current_user.lists.all, include: []
+      lists = current_user.lists
+      core_lists = lists.where(type: 'core').order(:created_at).ids
+      custom_lists = lists.where(type: 'custom').order(:created_at).ids
+      sort_order = core_lists + custom_lists
+      render json: {lists: lists.map { |i| [i.id, i] }.to_h, meta: {sort_order: sort_order, num_lists: sort_order.count}}
     end
 
     def create
@@ -11,8 +15,10 @@ module Api::V1
     end
 
     def show
-      sort_order = current_user.lists.find(params[:id]).items.rank(:idx).ids
-      render json: current_user.lists.find(params[:id]), include: ['items'], meta: {sort_order: sort_order, num_items: sort_order.count}
+      items = current_user.lists.find(params[:id]).items
+      sort_order = items.rank(:idx).ids
+      badge_count = items.where(completed: false).count
+      render json: current_user.lists.find(params[:id]), include: ['items'], meta: {badge_count: badge_count, num_items: sort_order.count, sort_order: sort_order}
     end
 
     def update
--- app/serializers/list_serializer.rb
+++ app/serializers/list_serializer.rb
@@ -6,6 +6,10 @@ class ListSerializer < ActiveModel::Serializer
 
   # Return list items with item id as key for easier use with Redux
   def items
-    items = object.items.rank(:idx).map { |i| [i.id, i] }.to_h
+    object.items.rank(:idx).map { |i| [i.id, i] }.to_h
+  end
+
+  def badge_count
+    object.items.where(completed: false).count
   end
 end
